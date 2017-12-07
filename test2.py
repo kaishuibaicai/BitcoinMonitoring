960b885b67c2a25cbf4ea5dadd3647b97cddb48f
徐梓琅 <272251416@qq.com>
2017-12-07 15:19:47 +0800

    sc

--- test2.py
+++ test2.py
@@ -5,4 +5,11 @@ from lamda import *
 client = Client(AK, AS)
 
 depth = client.get_order_book(symbol='IOTAETH')
-print depth
+#print depth
+
+prices = client.get_all_tickers()
+subscriberList = ['EOSBTC', 'EOSETH', 'IOTABTC', 'IOTAETH', 'CDTBTC', 'CDTETH', 'BTCUSDT', 'ETHUSDT']
+#print prices
+for item in prices:
+	if item['symbol'] in subscriberList :
+		print 'coinType: {}   pirce: {}'.format(item['symbol'],float(item['price']))
